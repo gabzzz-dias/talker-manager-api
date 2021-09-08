6c07d563cdfbd222c6d01ad9537084b8a2375a0f
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-09-07 23:28:42 -0300

    again

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (_request, response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Online in port ${PORT}`);
+  console.log('Online');
 });
 
 app.get('/talker', async (req, res) => {
