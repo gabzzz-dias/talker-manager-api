91c161ce52e843c2c6ca41d1a5f145c7ee67b5e3
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-09-11 14:15:44 -0300

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const talkersObj = ('./talker.json');  
 
