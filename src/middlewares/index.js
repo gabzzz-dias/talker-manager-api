d36bc40d1374427cd292b55f8f13ff50a746e3be
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-09-11 14:52:00 -0300

    ok

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,28 +1,32 @@
 const crypto = require('crypto');
 
-const generateToken = () => { 
-  crypto.randomBytes(8).toString('hex');
+const generateToken = (req, res) => {
+  const randomToken = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ randomToken });
 };
 
-const emailValidator = (email) => {
-  const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-  const emailFormat = regex.test(email);
-
-  if (!email.length) {
-    return { status: 400, msg: 'O campo "email" é obrigatório' };
-  } 
-  if (!emailFormat) {
-    return { status: 400, msg: 'O "email" deve ter o formato "email@email.com"' };
+const emailValidator = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
+  const re = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  const validEmail = re.test(email);
+  if (!validEmail) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
+  next();
 };
 
-const passwordValidator = (password) => {
-  if (!password.length) {
-    return { status: 400, msg: 'O campo "password" é obrigatório' };
+const passwordValidator = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é origatório' });
   }
   if (password.length < 6) {
-    return { status: 400, msg: 'O "password" deve ter pelo menos 6 caracteres' }; 
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
+  next();
 };
 
 module.exports = {
